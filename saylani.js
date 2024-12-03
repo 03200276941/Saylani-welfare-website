168d68cbfc0837aeebc967e120c63931037ffa9e
MUHAMMAD BAKHTAWAR KHAN <bakhtawark085@gmail.com>
2024-12-04 03:48:04 +0500

    added pictures

---
+++ saylani.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Daily Meals,', 'Monthly family support,', 'Education,', ' IT and vocational tranings,','clean water,', 'assistance to disaster affectees,', 'healthcare and lot more.', 'Web Designer'],
+    typeSpeed: 80,        
+    backSpeed: 80,       
+    backDelay: 1200,    
+    loop: true            
+});
